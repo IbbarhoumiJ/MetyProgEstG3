e4b26c2a8a83a977dd19533a6c41886d3d3278f0
Laboratorio C205 <labc205@uam.net.ni>
2024-08-19 08:30:19 -0600

    sesion 1

---
+++ HolaUam/HolaUam/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HolaUam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola desde la UAM");
+            Console.WriteLine("Mi nombre es Ibraaaa");
+            Console.WriteLine("Tengo 19 años");
+            Console.WriteLine("Me gusta comer hamburguesas");        }
+
+    }
+}
